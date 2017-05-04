bc889d47ea6417379b3dab294589869caa11dda2
antonio-calculon <calcul0n@protonmail.com>
2017-05-04 14:14:27 +0200

    added the REF, CLEAR and XCLEAR macros

--- src/cppobject.hpp
+++ src/cppobject.hpp
@@ -5,6 +5,22 @@
 
 
 
+#define REF(obj) ((typeof(obj)) (obj)->ref())
+
+#define CLEAR(obj) do {                         \
+    cppobject::Object *__tmp_obj_clear =        \
+      (cppobject::Object *) (obj);              \
+    (obj) = NULL;                               \
+    __tmp_obj_clear->unref();                   \
+  } while (0)
+
+#define XCLEAR(obj) do {                        \
+    if (obj)                                    \
+      CLEAR(obj);                               \
+  } while (0)
+
+
+
 // cppobject::Object
 //
 namespace cppobject
